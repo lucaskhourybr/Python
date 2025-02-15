14dd0c5a74e1932f4a61bbc734636a803854fd39
Lucas Khoury <94236030+lucaskhourybr@users.noreply.github.com>
2025-02-15 12:10:36 -0300

    Create Exercício_013.py

---
+++ Exercícios/Exercício_013.py
@@ -0,0 +1,21 @@
+# @title Exercício 013 - Estrutura de decisão aninhada com elif
+# Solicite um número ao usuário e teste se ele é par ou impar
+# Exiba o resultado 
+
+print("Inicio do programa \n")
+print("Bem vindo ao nosso programa , para descobrir se um número é par ou impar.\n")
+
+valor = int(input("Digite um número \n"))
+
+if (valor % 2) == 0:
+    print(f"O valor inserido foi {valor} , logo ele é PAR \n")
+
+else:
+
+    print(f"O valor inserido foi {valor} , logo ele é IMPAR \n") 
+
+
+print(f"|-------------------------------------------------|")
+print(f"|---------------Fim programa ---------------------|")
+print(f"|---------Denvolvido por LucasKhoury--------------|")
+print(f"|-------------------------------------------------|")
