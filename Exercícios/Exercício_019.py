ee99b02a1b05f58336bddb7736faec377d87fe89
Lucas Khoury <94236030+lucaskhourybr@users.noreply.github.com>
2025-02-15 16:01:19 -0300

    Create Exercício_019.py

---
+++ Exercícios/Exercício_019.py
@@ -0,0 +1,9 @@
+# @title Exercício 019 - Estrutura de repetição FOR com string agora 
+# Utilizando método repetição para verificar a palavra inteira
+# no caso for x in frase: quer dizer que x é cada letra da palavra exemplo Ola Senai
+# frase quer dizer a nossa palavra 
+
+frase = "Ola Senai"
+
+for x in frase:
+  print(x)
